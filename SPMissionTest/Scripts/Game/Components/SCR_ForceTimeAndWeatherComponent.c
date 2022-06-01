402c37698d8e561a155bc17b4e3fe2a7a7f8571f
Daniel Lewis <Biscuits101@users.noreply.github.com>
2022-06-02 00:48:11 +0100

    Add files via upload

--- SPMissionTest/Scripts/Game/Components/SCR_ForceTimeAndWeatherComponent.c
+++ SPMissionTest/Scripts/Game/Components/SCR_ForceTimeAndWeatherComponent.c
@@ -1,3 +1,6 @@
+//Custom script by Zeal, based on ex0's C&H variant
+//Modified and adjusted by Biscuits
+
 [ComponentEditorProps(category: "GameScripted/GameMode", description: "Forced time and weather controller.")]
 class SCR_ForceTimeAndWeatherComponentClass : SCR_BaseGameModeComponentClass
 {
@@ -13,21 +16,52 @@ class SCR_ForceTimeAndWeatherComponent : SCR_BaseGameModeComponent
 	//! Manager singleton instance, assigned on first get call
 	private static SCR_ForceTimeAndWeatherComponent s_pInstance;
 
-	//! If enabled custom weather Id will be used on session start. Authority only.
-	[Attribute(defvalue: "0", desc: "If enabled, custom weather Id will be used. Authority only.", category: "CaptureAndHold: Environment")]
-	protected bool m_bUseCustomWeather;
-
-	//! Weather IDs are the same as used in the TimeAndWeatherManager. Weather set on game start. Authority only.
-	[Attribute(defvalue: "", desc: "Weather IDs are the same as used in the TimeAndWeatherManager. Weather set on game start. Authority only.", category: "CaptureAndHold: Environment")]
-	protected string m_sCustomWeatherId;
+	//! If enabled custom date will be used on session start. Authority only.
+	[Attribute(defvalue: "1", desc: "If enabled, custom date will be used. Authority only.", category: "ON/OFF")]
+	protected bool m_bUseCustomDate;
 
 	//! If enabled custom time of the day will be used on session start. Authority only.
-	[Attribute(defvalue: "0", desc: "If enabled, custom time of the day will be used. Authority only.", category: "CaptureAndHold: Environment")]
+	[Attribute(defvalue: "1", desc: "If enabled, custom time of the day will be used. Authority only.", category: "ON/OFF")]
 	protected bool m_bUseCustomTime;
 
+	//! If enabled custom weather Id will be used on session start. Authority only.
+	[Attribute(defvalue: "1", desc: "If enabled, custom weather Id will be used. Authority only.", category: "ON/OFF")]
+	protected bool m_bUseCustomWeather;
+
+	//! If enabled custom weather Id will be used on session start. Authority only.
+	[Attribute(defvalue: "1", desc: "If enabled, custom Latitude/Longitude will be used. Authority only.", category: "ON/OFF")]
+	protected bool m_bUseCustomLatitudeLongitude;
+
+	//! Year set on game start. Authority only.
+	[Attribute(defvalue: "1989", UIWidgets.Slider, desc: "Year set on game start. Authority only.", category: "SETTINGS", params: "1900 2200 1")]
+	protected int m_iCustomYear;
+
+	//! Month set on game start. Authority only.
+	[Attribute(defvalue: "7", UIWidgets.Slider, desc: "Month set on game start. Authority only.", category: "SETTINGS", params: "1 12 1")]
+	protected int m_iCustomMonth;
+
+	//! Day set on game start. Authority only.
+	[Attribute(defvalue: "24", UIWidgets.Slider, desc: "Day set on game start. Authority only.", category: "SETTINGS", params: "1 31 1")]
+	protected int m_iCustomDay;
+
 	//! Time of the day set on game start. Authority only.
-	[Attribute(defvalue: "12", desc: "Time of the day set on game start. Authority only.", category: "CaptureAndHold: Environment", params: "0 24 0.01")]
+	[Attribute(defvalue: "9.6", UIWidgets.Slider, desc: "Time of the day set on game start. Authority only.", category: "SETTINGS", params: "0 24 0.01")]
 	protected float m_fCustomTimeOfTheDay;
+
+	//! Weather IDs are the same as used in the TimeAndWeatherManager. Weather set on game start. Authority only.
+	[Attribute(defvalue: "Clear", UIWidgets.ComboBox, desc: "Weather set on game start. Authority only.", category: "SETTINGS", enums: { ParamEnum("Clear", "Clear"), ParamEnum("Cloudy", "Cloudy"), ParamEnum("Overcast", "Overcast"), ParamEnum("Rainy", "Rainy") })]
+	protected string m_sCustomWeather;
+
+	//! Latitude set on game start. Authority only.
+	[Attribute(defvalue: "-4", UIWidgets.Slider, desc: "Latitude set on game start. Authority only.", category: "SETTINGS", params: "-90 90 0.01")]
+	protected float m_fCustomLatitude;
+
+	//! Longitude set on game start. Authority only.
+	[Attribute(defvalue: "71", UIWidgets.Slider, desc: "Longitude set on game start. Authority only.", category: "SETTINGS", params: "-180 180 0.01")]
+	protected float m_fCustomLongitude;
+
+	//! Reference to entity responsible for managing time and weather
+	protected TimeAndWeatherManagerEntity m_pTimeAndWeatherManager;
 
 	//------------------------------------------------------------------------------------------------
 	/*!
@@ -47,11 +81,29 @@ class SCR_ForceTimeAndWeatherComponent : SCR_BaseGameModeComponent
 	{
 		super.EOnInit(owner);
 
+		// Only on Authority
+		if (!m_pGameMode.IsMaster())
+			return;
+
+		// Is TimeAndWeatherManager entity available
+		m_pTimeAndWeatherManager = GetGame().GetTimeAndWeatherManager();
+		if (!m_pTimeAndWeatherManager)
+		{
+			Print("Cannot initialize TimeAndWeatherManagerEntity not found!", LogLevel.WARNING);
+			return;
+		}
+
+		if (m_bUseCustomDate)
+			SetDate(m_iCustomYear, m_iCustomMonth, m_iCustomDay);
+
 		if (m_bUseCustomTime)
 			SetTimeOfTheDay(m_fCustomTimeOfTheDay);
 
 		if (m_bUseCustomWeather)
-			SetWeather(m_sCustomWeatherId);
+			SetWeather(m_sCustomWeather);
+
+		if (m_bUseCustomLatitudeLongitude)
+			SetLatLong(m_fCustomLatitude, m_fCustomLongitude);
 	}
 
 	//------------------------------------------------------------------------------------------------
@@ -72,24 +124,11 @@ class SCR_ForceTimeAndWeatherComponent : SCR_BaseGameModeComponent
 
 	//------------------------------------------------------------------------------------------------
 	/*!
-		Forcefully sets weather to provided weatherId. Authority only.
+		Forcefully sets time of the date to provided value. Authority only.
 	*/
-	void SetWeather(string weatherId)
+	void SetDate(int year, int month, int day)
 	{
-		if (!m_pGameMode.IsMaster())
-			return;
-
-		if (weatherId.IsEmpty())
-			return;
-
-		TimeAndWeatherManagerEntity weatherManager = GetGame().GetTimeAndWeatherManager();
-		if (!weatherManager)
-		{
-			Print("Cannot initialize weather: TimeAndWeatherManagerEntity not found!", LogLevel.WARNING);
-			return;
-		}
-
-		weatherManager.ForceWeatherTo(true, weatherId, 0.0);
+		m_pTimeAndWeatherManager.SetDate(year, month, day, true);
 	}
 
 	//------------------------------------------------------------------------------------------------
@@ -98,16 +137,28 @@ class SCR_ForceTimeAndWeatherComponent : SCR_BaseGameModeComponent
 	*/
 	void SetTimeOfTheDay(float timeOfTheDay)
 	{
-		if (!m_pGameMode.IsMaster())
+		m_pTimeAndWeatherManager.SetTimeOfTheDay(timeOfTheDay, true);
+	}
+
+	//------------------------------------------------------------------------------------------------
+	/*!
+		Forcefully sets weather to provided weatherId. Authority only.
+	*/
+	void SetWeather(string weatherId)
+	{
+		if (weatherId.IsEmpty())
 			return;
 
-		TimeAndWeatherManagerEntity weatherManager = GetGame().GetTimeAndWeatherManager();
-		if (!weatherManager)
-		{
-			Print("Cannot initialize TimeOfTheDay: TimeAndWeatherManagerEntity not found!", LogLevel.WARNING);
-			return;
-		}
+		m_pTimeAndWeatherManager.ForceWeatherTo(true, weatherId, 0.0);
+	}
 
-		weatherManager.SetTimeOfTheDay(timeOfTheDay, true);
+	//------------------------------------------------------------------------------------------------
+	/*!
+		Forcefully sets latitude/longitude to provided values. Authority only.
+	*/
+	void SetLatLong(float latitude, float longitude)
+	{
+		m_pTimeAndWeatherManager.SetCurrentLatitude(latitude);
+		m_pTimeAndWeatherManager.SetCurrentLongitude(longitude);
 	}
 }
